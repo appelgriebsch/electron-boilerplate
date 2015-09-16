cccef474fbe1cf760fb435fd29d411251cf8e7c1
Andreas Gerlach <andreas.gerlach@smail.fh-koeln.de>
2015-09-16 22:36:11 +0200

    initial grunt config to package linux versions of the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,9 +45,77 @@ grunt.initConfig({
         ignore: 'pkg'
       }
     }
+  },
+
+  'electron-redhat-installer': {
+    options: {
+      productName: 'Boilerplate',
+      productDescription: 'An Electron boilerplate project.',
+      icon: 'app/assets/boilerplate.png',      
+      categories: [
+        'Utility'
+      ],
+      rename: function(dest, src) {
+        return dest + '<%= name %>-<%= version %>-<%= revision %>.<%= arch %>.rpm';
+      }
+    },
+
+    linux32: {
+      options: {
+        arch: 'x86'
+      },
+      src: 'pkg/boilerplate-linux-ia32/',
+      dest: 'pkg/installer/'
+    },
+
+    linux64: {
+      options: {
+        arch: 'x86_64'
+      },
+      src: 'pkg/boilerplate-linux-x64/',
+      dest: 'pkg/installer/'
+    }
+  },
+
+  'electron-debian-installer': {
+    options: {
+      productName: 'Boilerplate',
+      productDescription: 'An Electron boilerplate project.',
+      icon: 'app/assets/boilerplate.png',
+      section: 'devel',
+      priority: 'optional',
+      lintianOverrides: [
+        'changelog-file-missing-in-native-package',
+        'executable-not-elf-or-script',
+        'extra-license-file'
+      ],
+      categories: [
+        'Utility'
+      ],
+      rename: function(dest, src) {
+        return dest + '<%= name %>_<%= version %>-<%= revision %>_<%= arch %>.deb';
+      }
+    },
+
+    linux32: {
+      options: {
+        arch: 'i386'
+      },
+      src: 'pkg/boilerplate-linux-ia32/',
+      dest: 'pkg/installer/'
+    },
+
+    linux64: {
+      options: {
+        arch: 'amd64'
+      },
+      src: 'pkg/boilerplate-linux-x64/',
+      dest: 'pkg/installer/'
+    }
   }
+
 });
 
 grunt.registerTask('osx', ['electron:osxBuild']);
 grunt.registerTask('win32', ['electron:win32Build']);
-grunt.registerTask('linux', ['electron:linuxBuild']);
+grunt.registerTask('linux', ['electron:linuxBuild', 'electron-redhat-installer', 'electron-debian-installer']);
