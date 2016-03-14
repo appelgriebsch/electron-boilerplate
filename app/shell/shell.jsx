1c84121d6a1263eb0cc5d0588faf685d68d80295
Andreas Gerlach <andreas.gerlach@smail.fh-koeln.de>
2016-03-14 16:11:49 +0100

    shell layout progress in grommet

--- app/shell/shell.jsx
+++ app/shell/shell.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import electron from 'electron'
-import { App, Split, Sidebar, Header, Article, Section } from 'grommet'
+import { App, Split, Sidebar, Header, Article, Section, Icons, Box, Title, Menu } from 'grommet'
 
 const appCfg = electron.remote.app.sysConfig();
 
@@ -10,11 +10,19 @@ export default class Shell extends React.Component {
     return (
        <App centered={false}>
          <Split flex="right" fixed={true}>
-           <Sidebar size="small" fixed={true} primary={true}>
-            <Header>Navigation</Header>
+           <Sidebar size="small" colorIndex="grey-1" fixed={true}>
+              <Header pad={{horizontal: 'small'}}>
+                <Icons.Base.Menu/>
+              </Header>
+              <Menu>
+              </Menu>
            </Sidebar>
            <Article>
-            <Header>Content</Header>
+            <Header size="small" justify="between" colorIndex="grey-1" pad="small">
+              <Title>
+                { appCfg.app.name } { appCfg.app.version }
+              </Title>
+            </Header>
             <Section>{ this.props.children }</Section>
            </Article>
          </Split>
