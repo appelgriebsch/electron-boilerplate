877eec20cd5dbd513fa0c653ebd7cad0f268784d
Andreas Gerlach <andi@appelgriebsch.org>
2016-03-23 15:05:32 +0100

    shell layout upgrade

--- app/shell/shell.jsx
+++ app/shell/shell.jsx
@@ -8,23 +8,23 @@ export default class Shell extends React.Component {
   render() {
     return (
        <App centered={false}>
-         <Split flex="right" fixed={true}>
-           <Sidebar size="small" colorIndex="grey-1" fixed={true}>
-              <Header pad={{horizontal: 'small'}}>
-                <Icons.Base.Menu/>
-              </Header>
-              <Menu>
-              </Menu>
-           </Sidebar>
-           <Article>
-            <Header size="small" justify="between" colorIndex="grey-1" pad="small">
-              <Title>
-                { appCfg.app.name } { appCfg.app.version }
-              </Title>
-            </Header>
-            <Section>{ this.props.children }</Section>
-           </Article>
-         </Split>
+          <Header size="small" justify="between" colorIndex="grey-1" pad="small">
+            <Title>
+              { appCfg.app.name } { appCfg.app.version }
+            </Title>
+          </Header>
+          <Split flex="right" fixed={true}>
+            <Sidebar size="small" colorIndex="grey-1" fixed={true}>
+                <Header pad={{horizontal: 'small'}}>
+                  <Icons.Base.Menu/>
+                </Header>
+                <Menu>
+                </Menu>
+            </Sidebar>
+            <Article>
+              <Section>{ this.props.children }</Section>
+            </Article>
+          </Split>
        </App>
     );
   };
