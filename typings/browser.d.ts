893d124d1f59d7a80f97d2cb92a71e4db1f269c8
Andreas Gerlach <andi@appelgriebsch.org>
2016-06-08 14:56:56 +0200

    clean up obsolete stuff, increase package dependency versions

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/angular-material/index.d.ts" />
-/// <reference path="browser/ambient/angular/index.d.ts" />
-/// <reference path="browser/ambient/github-electron/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/pouchdb/pouch/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/angular-material/index.d.ts" />
-/// <reference path="main/ambient/angular/index.d.ts" />
-/// <reference path="main/ambient/github-electron/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/pouchdb/pouch/index.d.ts" />
